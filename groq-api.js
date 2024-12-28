bd0e90926964996840364d64072e33039d61136f
sarahxgh <sarah.mahmoudi@ensia.edu.dz>
2024-12-28 20:34:32 +0100

    nodejs initial commit

---
+++ groq-api.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import Groq from "groq-sdk";
+import cors from 'cors';
+
+const groqApiKey = "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"
+const groq = new Groq({ apiKey: groqApiKey });
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.post('/quiz-data', async (req, res) => {
+  try {
+    // Get the prompt from the request body
+    const { prompt } = req.body;
+    console.log('Received prompt:', req.body.prompt);
+    if (!prompt) {
+      return res.status(400).json({ error: 'Prompt is required' });
+    }
+
+
+    // Query Groq with the received prompt
+    const quizData = await groq.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+        model: 'llama-3.1-70b-versatile',
+      });
+
+    // Send the quiz data back as JSON
+    res.json(quizData["choices"][0]["message"]["content"]);
+  } catch (error) {
+    console.error('Error fetching quiz data:', error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
